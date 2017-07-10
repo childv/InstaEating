73ee022dec604cf47b85b96ebe69e1c5ef2abe3d
Veronica <vachild@me.com>
2017-07-10 17:05:42 -0400

    Starting the scraper

---
+++ instagram-search.py
@@ -0,0 +1,25 @@
+# instagram-search.py
+# A program that grabs the most common posts on restaurants
+# Veronica Child
+# July 10, 2017
+
+# Code that scrapes Instagram's explore page to get the latest posts regarding food
+
+import requests
+import json
+from bs4 import BeautifulSoup
+
+class InstagramExploreSearch(hashtag):
+	"""
+	Class that mines the instagram explore page with a given hashtag.
+	"""
+
+	def __init__(self):
+		super().__init__()
+
+	def save_results(self, post_results):
+		super
+
+if __name__ == '__main__':
+
+
