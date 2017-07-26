6c9544b0b19dbfa0053d74efb2719c7eda475c5a
Veronica <vachild@me.com>
2017-07-26 08:31:45 -0400

    Count at signs

--- postAnalyzer.py
+++ postAnalyzer.py
@@ -19,28 +19,46 @@ def remove_tags(dirty_str):
 	cleaned_str = re.sub('(@[\w.]+)|(#[\w.]*)', '', dirty_str)
 	return cleaned_str
 
+# Question: What are the most frequently occuring @ signs?
+# param: data_file 	JSON-format file that contains instagram post data
+# return:	Counter object of all at signs
+def get_frequent_ats(data_file):
+	count_ats = Counter()
+
+	with open(data_file, "r") as json_file:
+		for line in json_file:
+			post_json = json.loads(line)
+			count_ats.update(post_json['at_signs'])
+	
+	return count_ats
+
 # Question: What are the most frequently occurring words?
 def tokenize_json(data_file):
-	json_data= []
 	tok_captions = [] # list of tokens from captions
+	count_all = Counter()
 
 	# Iterate through JSON data
 	with open(data_file, "r") as json_file:
 		for line in json_file:
 		 	post_json = json.loads(line)
 		 	# Add to list of dictionaries
-		 	json_data.append(post_json)
+		 	#json_data.append(post_json)
 
 		 	# Create doc from cleaned caption (no @ or #)
-		 	caption = remove_tags(post_json['caption'])
-		 	doc_caption = nlp(post_json['caption'])
+		 	clean_caption = remove_tags(post_json['caption'])
+		 	doc_caption = nlp(clean_caption)
 		 	#print doc_caption
 
+		 	# Create list from cleaned caption
+		 	terms_all = [term for term in clean_caption]
+		 	# Update counter
+		 	count_all.update(terms_all)
+
 		 	# Iterate through tokens in caption
-		 	for token in doc_caption:
-		 		tok_captions.append(token)
-		 		#print token
-		 		pass
+		 	# for token in doc_caption:
+		 	# 	tok_captions.append(token)
+		 	# 	#print token
+		 	# 	pass
 
 
 		 	#parsed_captions.append(parsed_caption)
@@ -48,6 +66,7 @@ def tokenize_json(data_file):
 		 	#tokens_all =
 
 		# count_all = Counter()
+	print(count_all.most_common(5))
 
 	return doc_caption
  
@@ -58,22 +77,24 @@ if __name__ == '__main__':
 	data_file = "data/raw.json"
 
 
-	parsed_data = tokenize_json(data_file)
+	#parsed_data = tokenize_json(data_file)
+	counter_ats = get_frequent_ats(data_file)
+	print(parsed_ats.most_common(5))
 
 	try:
 		# Let's look at the sentences
 		sents = []
-		# the "sents" property returns spans
-		# spans have indices into the original string
-		# where each index value represents a token
-		for span in parsed_data.sents:
-    		# go from the start to the end of each span, returning each token in the sentence
-    		# combine each token using join()
-    			sent = ''.join(parsedData[i].string for i in range(span.start, span.end)).strip()
-    		sents.append(sent)
+		# # the "sents" property returns spans
+		# # spans have indices into the original string
+		# # where each index value represents a token
+		# for span in parsed_data.sents:
+  #   		# go from the start to the end of each span, returning each token in the sentence
+  #   		# combine each token using join()
+  #   			sent = ''.join(parsedData[i].string for i in range(span.start, span.end)).strip()
+  #   		sents.append(sent)
 
-		for sentence in sents:
-		    print(sentence)
+		# for sentence in sents:
+		#     print(sentence)
 
 	except TypeError:
 		print("Parsed data is not iterable. Is there a problem with the data?")
