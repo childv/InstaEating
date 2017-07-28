724318e3a1472dcca206d21c98d009df566a1757
Veronica <vachild@me.com>
2017-07-28 12:02:36 -0400

    Start data analysis with pandas. NLP on hold

--- postAnalyzer.py
+++ postAnalyzer.py
@@ -7,6 +7,7 @@ from collections import Counter
 import json
 import spacy
 import re
+import pandas as pd
 
 global nlp
 # caption_data = ( item['value'] for item in test_data )
@@ -60,39 +61,65 @@ def tokenize_json(data_file):
 		 	# 	#print token
 		 	# 	pass
 
-
-		 	#parsed_captions.append(parsed_caption)
-		 	#parsed_captions += parsed_caption
-		 	#tokens_all =
-
-		# count_all = Counter()
 	print(count_all.most_common(5))
 
 	return doc_caption
  
+ # Class holder for all things pandas 
+ class pandasAnalyzer():
+ 	# Question: When a post has likes above the mean, are the number of hashtags above the mean too? (or vice versa)
+ 	# returns: 	number of posts that have number of likes and hashtags above the mean,
+ 	#			number of posts that have number of likes and hashtags in different direction
+ 	def series_test():
+ 		like_values = []
+ 		num_hashtags_values = []
+
+ 		# Iterate through JSON data
+		with open(data_file, "r") as json_file:
+			for line in json_file:
+				post_json = json.loads(line)
+				like_values.append(post_json['likes'])
+				num_hashtags_values.append(len(post_json['hashtags']))
+
+		variable1 = pd.Series(like_values) # convert list of likes into pandas series
+		variable2 = pd.Series(num_hashtags_values) # convert list of no. of hashtags into pandas series
+
+		both_above = (variable1 > variable1.mean()) & (variable2 > variable2.mean())
+		both_below = (variable1 < variable1.mean()) & (variable2 < variable2.mean())
+
+		is_same_direction = both_above | both_below
+		num_same_direction = is_same_direction.sum()
+
+		# calulate different direction by taking the difference
+		num_different_direction = len(variable1) - num_same_direction
+
+		return (num_same_direction, num_different_direction)
 
 if __name__ == '__main__':
-	# Process English captions
-	nlp = spacy.load('en') # preload
 	data_file = "data/raw.json"
 
+	analyzer = pandasAnalyzer()
+
+
+	# Process English captions
+	#nlp = spacy.load('en') # preload
 
 	#parsed_data = tokenize_json(data_file)
-	counter_ats = get_frequent_ats(data_file)
-	print(parsed_ats.most_common(5))
+	#counter_ats = get_frequent_ats(data_file)
+	#print(parsed_ats.most_common(5))
 
 	try:
+		# Taken from: https://nicschrading.com/project/Intro-to-NLP-with-spaCy/
 		# Let's look at the sentences
 		sents = []
 		# # the "sents" property returns spans
 		# # spans have indices into the original string
 		# # where each index value represents a token
 		# for span in parsed_data.sents:
-  #   		# go from the start to the end of each span, returning each token in the sentence
-  #   		# combine each token using join()
-  #   			sent = ''.join(parsedData[i].string for i in range(span.start, span.end)).strip()
-  #   		sents.append(sent)
-
+    		# go from the start to the end of each span, returning each token in the sentence
+     		# combine each token using join()
+     		# sent = ''.join(parsedData[i].string for i in range(span.start, span.end)).strip()
+     		# sents.append(sent)
 		# for sentence in sents:
 		#     print(sentence)
 
@@ -100,23 +127,4 @@ if __name__ == '__main__':
 		print("Parsed data is not iterable. Is there a problem with the data?")
 		sys.exit(1)
 
-		# Let's look at the tokens
-		# All you have to do is iterate through the parsedData
-		# Each token is an object with lots of different properties
-		# A property with an underscore at the end returns the string representation
-		# while a property without the underscore returns an index (int) into spaCy's vocabulary
-		# The probability estimate is based on counts from a 3 billion word
-		# corpus, smoothed using the Simple Good-Turing method.
-		# for i, token in enumerate(parsed_data):
-		#     # print("original:", token.orth, token.orth_)
-		#     # print("lowercased:", token.lower, token.lower_)
-		#     # print("lemma:", token.lemma, token.lemma_)
-		#     # print("shape:", token.shape, token.shape_)
-		#     # print("prefix:", token.prefix, token.prefix_)
-		#     # print("suffix:", token.suffix, token.suffix_)
-		#     # print("log probability:", token.prob)
-		#     # print("Brown cluster id:", token.cluster)
-		#     # print("----------------------------------------")
-		#     if i > 1:
-		#     	break
 
